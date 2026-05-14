8ac132ff5ad13986d180f2b03181f6df672af1b9
erik <erik@erik.xyz>
2026-05-14 17:49:31 +0800

    feat: add KV store API module

---
+++ src/Api/Kv.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Erikwang\Consul\Api;
+
+use Erikwang\Consul\Transport\TransportInterface;
+
+class Kv
+{
+    private TransportInterface $transport;
+
+    public function __construct(TransportInterface $transport)
+    {
+        $this->transport = $transport;
+    }
+
+    public function get(string $key, array $options = []): ?array
+    {
+        $query = $this->buildQuery($options);
+        $result = $this->transport->get("/v1/kv/{$key}", $query);
+        return !empty($result) ? $result[0] : null;
+    }
+
+    public function all(string $prefix = '', array $options = []): array
+    {
+        $query = $this->buildQuery($options);
+        return $this->transport->get("/v1/kv/{$prefix}", array_merge($query, ['recurse' => 'true']))
+            ?? [];
+    }
+
+    public function put(string $key, string $value, array $options = []): bool
+    {
+        $response = $this->transport->put("/v1/kv/{$key}", ['value' => base64_encode($value)], $options);
+        return ($response['body'] ?? null) === true || $response === ['body' => true];
+    }
+
+    public function delete(string $key, array $options = []): bool
+    {
+        $this->transport->delete("/v1/kv/{$key}", $options);
+        return true;
+    }
+
+    public function keys(string $prefix = '', string $separator = ''): array
+    {
+        $query = [];
+        if ($separator !== '') {
+            $query['separator'] = $separator;
+        }
+        $result = $this->transport->get("/v1/kv/{$prefix}", array_merge($query, ['keys' => 'true']));
+        return $result ?? [];
+    }
+
+    private function buildQuery(array $options): array
+    {
+        $query = [];
+        if (isset($options['dc']))          $query['dc'] = $options['dc'];
+        if (isset($options['index']))       $query['index'] = $options['index'];
+        if (isset($options['wait']))        $query['wait'] = $options['wait'];
+        if (isset($options['ns']))          $query['ns'] = $options['ns'];
+        if (isset($options['partition']))   $query['partition'] = $options['partition'];
+        if (isset($options['raw']))         $query['raw'] = 'true';
+        if (isset($options['cas']))         $query['cas'] = $options['cas'];
+        return $query;
+    }
+}
---
+++ tests/Api/KvTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Erikwang\Consul\Tests\Api;
+
+use Erikwang\Consul\Api\Kv;
+use Erikwang\Consul\Transport\TransportInterface;
+use PHPUnit\Framework\TestCase;
+
+class KvTest extends TestCase
+{
+    private $transport;
+    private Kv $kv;
+
+    protected function setUp(): void
+    {
+        $this->transport = $this->createMock(TransportInterface::class);
+        $this->kv = new Kv($this->transport);
+    }
+
+    public function testGetReturnsValue(): void
+    {
+        $this->transport->method('get')
+            ->with('/v1/kv/config/app', [])
+            ->willReturn([['Key' => 'config/app', 'Value' => base64_encode('hello')]]);
+
+        $result = $this->kv->get('config/app');
+
+        $this->assertSame('Key', array_key_first($result));
+        $this->assertSame('config/app', $result['Key']);
+    }
+
+    public function testGetReturnsNullWhenMissing(): void
+    {
+        $this->transport->method('get')
+            ->with('/v1/kv/missing', [])
+            ->willReturn([]);
+
+        $result = $this->kv->get('missing');
+
+        $this->assertNull($result);
+    }
+
+    public function testAllReturnsRecursiveList(): void
+    {
+        $this->transport->method('get')
+            ->with('/v1/kv/config/', ['recurse' => 'true'])
+            ->willReturn([
+                ['Key' => 'config/app', 'Value' => base64_encode('a')],
+                ['Key' => 'config/db', 'Value' => base64_encode('b')],
+            ]);
+
+        $result = $this->kv->all('config/');
+
+        $this->assertCount(2, $result);
+    }
+
+    public function testPutEncodesValue(): void
+    {
+        $this->transport->method('put')
+            ->with('/v1/kv/key', ['value' => base64_encode('data')], [])
+            ->willReturn(['body' => true]);
+
+        $result = $this->kv->put('key', 'data');
+
+        $this->assertTrue($result);
+    }
+
+    public function testKeysReturnsKeyList(): void
+    {
+        $this->transport->method('get')
+            ->with('/v1/kv/config/', ['keys' => 'true'])
+            ->willReturn(['config/app', 'config/db']);
+
+        $result = $this->kv->keys('config/');
+
+        $this->assertSame(['config/app', 'config/db'], $result);
+    }
+
+    public function testDelete(): void
+    {
+        $this->transport->expects($this->once())
+            ->method('delete')
+            ->with('/v1/kv/key', []);
+
+        $result = $this->kv->delete('key');
+
+        $this->assertTrue($result);
+    }
+}
